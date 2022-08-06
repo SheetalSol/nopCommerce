f4fdcd501c518233a58422831c79064d2c3f0380
Sheetal Solanki <smsolanki@gmail.com>
2022-08-06 16:40:54 +0100

    Html report plugin added in testrunner

--- src/main/java/utils/BaseClass.java
+++ src/main/java/utils/BaseClass.java
@@ -3,14 +3,25 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseClass {
     public static WebDriver driver;
     public void openBrowser()
     {
-     WebDriverManager.chromedriver().setup();
-     driver = new ChromeDriver();
-     driver.manage().window().maximize();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        // Edge Driver Setup
+        /*WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();*/
+
+        /* WebDriverManager.operadriver().setup();
+        driver = new OperaDriver(); */
+
+       driver.manage().window().maximize();
     }
 
     public void closeBrowser()
